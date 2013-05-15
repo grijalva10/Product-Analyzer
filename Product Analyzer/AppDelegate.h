e540876ac8d5b340321b9022b7553d02d1f78a86
Jeff <grijalva10@gmail.com>
2013-05-15 14:20:35 -0700

    Initial Commit

---
+++ Product Analyzer/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Product Analyzer
+//
+//  Created by Jeff Grijalva on 5/15/13.
+//  Copyright (c) 2013 Jeff Grijalva. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Product Analyzer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Product Analyzer
+//
+//  Created by Jeff Grijalva on 5/15/13.
+//  Copyright (c) 2013 Jeff Grijalva. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
